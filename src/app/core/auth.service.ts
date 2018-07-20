d89d69c4649ef2e8ed917762b968d0945a8c2972
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-07-20 09:06:40 +0200

    -structure

---
+++ src/app/core/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { User } from 'app/user/user';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class AuthService {
+    currentUser: User;
+
+    constructor(private messageService: MessageService) { }
+
+    isLoggedIn(): boolean {
+        return !!this.currentUser;
+    }
+
+    login(userName: string, password: string): void {
+        if (!userName || !password) {
+            this.messageService.addMessage('Please enter your userName and password');
+            return;
+        }
+        if (userName === 'admin') {
+            this.currentUser = {
+                id: 1,
+                userName: userName,
+                isAdmin: true
+            };
+            this.messageService.addMessage('Admin login');
+            return;
+        }
+        this.currentUser = {
+            id: 2,
+            userName: userName,
+            isAdmin: false
+        };
+        this.messageService.addMessage(`User: ${this.currentUser.userName} logged in`);
+    }
+
+    logout(): void {
+        this.currentUser = null;
+    }
+}
--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MessagesComponent } from './messages/messages.component';
+import { MessagesComponent } from './messages.component';
 
 @NgModule({
   imports: [
