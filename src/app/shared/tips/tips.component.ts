42828656a83e290a6e73170e110f8072176e9ff7
henrikmathiesen <henrik_mathiesen@hotmail.com>
2018-08-11 19:11:57 +0200

    -gutter as rem -design -tooltip work in progress

--- src/app/shared/tips/tips.component.ts
+++ src/app/shared/tips/tips.component.ts
@@ -2,8 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-tips',
-    templateUrl: './tips.component.html'
+    templateUrl: './tips.component.html',
+    styleUrls: ['./tips.component.scss']
 })
 export class TipsComponent {
+
+    isVisible = false;
+
+    toggleVisible() {
+        this.isVisible = !this.isVisible;
+    }
 
 }
