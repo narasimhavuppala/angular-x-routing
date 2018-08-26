c11d3bd73ab254906aae90465ccebd2e2a42da3b
narasimhavuppala <nraovuppala@Wgmail.com>
2018-08-26 18:22:55 +0530

    updated base href

--- src/app/data/product-data.ts
+++ src/app/data/product-data.ts
@@ -3,10 +3,10 @@ import { ProductModel } from './product.model';
 export const productData: ProductModel[] = [
     {
         'id': 1,
-        'productName': 'Leaf Rake',
+        'productName': 'Pen',
         'productCode': 'GDN-0011',
         'releaseDate': 'March 19, 2016',
-        'description': 'Leaf rake with 48-inch wooden handle.',
+        'description': 'Pen description.',
         'price': 19.95,
         'starRating': 3.2,
         'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png',
@@ -15,10 +15,10 @@ export const productData: ProductModel[] = [
     },
     {
         'id': 2,
-        'productName': 'Garden Cart',
+        'productName': 'Book',
         'productCode': 'GDN-0023',
         'releaseDate': 'March 18, 2016',
-        'description': '15 gallon capacity rolling garden cart',
+        'description': 'Book Demo',
         'price': 32.99,
         'starRating': 4.2,
         'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png',
